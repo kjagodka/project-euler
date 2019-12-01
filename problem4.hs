898b1abddf1ea851ea2644acafddd7e3dd1c6669
cyryl <Cyryl Wakiewicz>
2019-12-01 22:57:28 +0100

    use maximum function instead of sorting

--- problem4.hs
+++ problem4.hs
@@ -6,4 +6,4 @@ isPalindrome n =
 
 main = do
   let products = [x * y | x <- [100..999], y <- [100..x]]
-  print $ last $ sort $ filter isPalindrome products
+  print $ maximum $ filter isPalindrome products
