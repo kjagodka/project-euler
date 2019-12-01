f88e1400170b3f60c08e62265077554a8c3fd9c4
cyryl <Cyryl Wakiewicz>
2019-12-02 00:39:38 +0100

    problem 10

---
+++ problem10.hs
@@ -0,0 +1,15 @@
+isPrime 0 = False
+isPrime 1 = False
+isPrime 2 = True
+isPrime n =
+  isPrimeHelper n primes
+  where
+     isPrimeHelper n (p:primes)
+       | p * p > n      = True
+       | n `mod` p == 0 = False
+       | otherwise        = isPrimeHelper n primes
+
+primes = filter isPrime [1..]
+
+main = do
+  print $ sum $ filter isPrime [1..2000000]
